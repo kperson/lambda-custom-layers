d568cd9f2f8b1163f37b3f6f4f03ac870e2a7e44
Kelton Person <keltonperson@Keltons-MacBook-Pro.local>
2019-07-03 06:46:26 -0500

    remove unneede code

--- swift5/example/Sources/main.swift
+++ swift5/example/Sources/main.swift
@@ -1,31 +1,15 @@
 //brew install libressl
 
-    import Vapor
+import Vapor
 import AWSLambdaAdapter
+import VaporLambdaAdapter
 
-class Handler: LambdaEventHandler {
-    func handle(data: [String : Any], eventLoopGroup: EventLoopGroup) -> EventLoopFuture<[String : Any]> {
-        return eventLoopGroup.next().newSucceededFuture(result: [
-            "hello": "word"
-        ])
-    }
-    
+let app = try Application(runAsLambda: true)
+let router = try app.make(Router.self)
+
+router.get("hello") { req in
+    return "Hello, world."
 }
 
-let dispatcher = LambdaEventDispatcher(handler: Handler())
-let server = dispatcher.start()
-try? server.wait()
-
-
-
-//import VaporLambdaAdapter
-
-//let app = try Application(runAsLambda: true)
-//let router = try app.make(Router.self)
-//
-//router.get("hello") { req in
-//    return "Hello, world."
-//}
-//
-//try app.run()
+try app.run()
 
--- swift5/run.sh
+++ swift5/run.sh
@@ -1,3 +1,3 @@
- #!/bin/bash
+#!/bin/sh
  
 /opt/swift-shared-libs/ld-linux-x86-64.so.2 --library-path /opt/swift-shared-libs/lib $LAMBDA_TASK_ROOT/$EXECUTABLE
